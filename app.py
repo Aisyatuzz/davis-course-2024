636afcd209774f2181579a6bedfa4c4226d1fb98
Aisyatuzz <107935563+Aisyatuzz@users.noreply.github.com>
2024-05-07 16:37:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-
+import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
+#1
 # reading the database
 data = pd.read_csv("https://raw.githubusercontent.com/Aisyatuzz/davis-course-2024/main/tips.csv")
 
-# Scatter plot with day against tip
-plt.scatter(data['day'], data['tip'])
+# Create a scatter plot
+fig, ax = plt.subplots()
+ax.scatter(data['day'], data['tip'])
 
 # Adding Title to the Plot
-plt.title("Scatter Plot")
+ax.set_title("Scatter Plot")
 
 # Setting the X and Y labels
-plt.xlabel('Day')
-plt.ylabel('Tip')
+ax.set_xlabel('Day')
+ax.set_ylabel('Tip')
 
-plt.show()
+# Display the plot in Streamlit
+st.pyplot(fig)
