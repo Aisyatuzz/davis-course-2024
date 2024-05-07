d9165a5d2d0796bf394e06a0727443657b3d65e1
Aisyatuzz <107935563+Aisyatuzz@users.noreply.github.com>
2024-05-07 17:07:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,24 +42,18 @@ fig = px.histogram(data, x='total_bill', color='sex', marginal='rug')
 st.plotly_chart(fig, use_container_width=True)
 
 #3
-df = px.data.gapminder()
-
+# Create scatter plot using Plotly Express
 fig = px.scatter(
-    df.query("year==2007"),
-    x="gdpPercap",
-    y="lifeExp",
-    size="pop",
-    color="continent",
-    hover_name="country",
-    log_x=True,
-    size_max=60,
+    data,
+    x="day",
+    y="tip",
+    color="size",
+    size="total_bill",
+    color_continuous_scale="reds",
 )
 
-tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
-with tab1:
-    # Use the Streamlit theme.
-    # This is the default. So you can also omit the theme argument.
+# Display the plot using Streamlit in one tab
+with st.expander("Plotly Chart", expanded=True):
+    # Use the Streamlit theme (default).
     st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-with tab2:
-    # Use the native Plotly theme.
-    st.plotly_chart(fig, theme=None, use_container_width=True)
+
