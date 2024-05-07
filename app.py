7b3d0793e7f0a72df935f65d209edf5df436f544
Aisyatuzz <107935563+Aisyatuzz@users.noreply.github.com>
2024-05-07 15:22:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import streamlit as st
 
 # reading the database
 data = pd.read_csv("https://github.com/Aisyatuzz/davis-course-2024/blob/main/tips.csv")
