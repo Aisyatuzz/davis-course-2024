7e28a05e12ac1d0ca956dc92f9463dbe4b0e3439
Aisyatuzz <107935563+Aisyatuzz@users.noreply.github.com>
2024-05-07 16:08:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 # reading the database
-data = pd.read_csv("https://raw.githubusercontent.com/Aisyatuzz/davis-course-2024/main/tips.csv")
+data = pd.read_csv
 
 # printing the top 10 rows
 display(data.head(10))
