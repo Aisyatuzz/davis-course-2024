f95bcf8cda9cc53dc402ba815ae56ded5a13515c
Aisyatuzz <107935563+Aisyatuzz@users.noreply.github.com>
2024-05-07 16:05:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,8 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import streamlit as st
 
 # reading the database
 data = pd.read_csv("https://raw.githubusercontent.com/Aisyatuzz/davis-course-2024/main/tips.csv")
 
-# Scatter plot with day against tip
-plt.scatter(data['day'], data['tip'], c=data['size'], s=data['total_bill'])
-
-# Adding Title to the Plot
-plt.title("Scatter Plot")
-
-# Setting the X and Y labels
-plt.xlabel('Day')
-plt.ylabel('Tip')
-
-# Adding Colorbar
-plt.colorbar(label='Size')
-
-# Displaying the plot in Streamlit
-st.pyplot()
+# printing the top 10 rows
+display(data.head(10))
