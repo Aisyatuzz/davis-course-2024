cea2b2e89550234b722f46fbe68e95d34b00a408
Aisyatuzz <107935563+Aisyatuzz@users.noreply.github.com>
2024-05-14 14:23:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,26 +5,6 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import os
 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
-
-
-st.bar_chart(chart_data)
-
-
-
-# Initialize connection.
-# conn = st.connection('mysql', type='sql', username=st.secrets["DB_USER"], password=st.secrets["DB_PASS"], host=st.secrets["HOST"], database=st.secrets["DB"])
-# conn = st.connection(**st.secrets.db_credentials)
-conn = st.connection("mydb", type="sql", autocommit=True)
-# st.write(st.secrets["connections.mydb"]["username"])
-# Perform query.
-df = conn.query('SELECT EnglishPromotionName, StartDate, EndDate, MaxQty from dimpromotion limit 10;', ttl=600)
-
-st.table(df)
-# Print results.
-# for row in df.itertuples():
-#     st.write(f"{row.EnglishPromotionName} , {row.MaxQty} ")
-
 # Menampilkan teks 
 st.subheader("Hello ^~^")
 st.subheader("")
