aaeefce6be8ea2994e3b22e484d712118ea62aa6
Aisyatuzz <107935563+Aisyatuzz@users.noreply.github.com>
2024-05-14 14:29:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
+import matplotlib.pyplot as plt
+import plotly.express as px
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import plotly.express as px
 import os
-
 
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
